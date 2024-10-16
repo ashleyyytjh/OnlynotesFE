74be4e9edc923a5ede4887ef04245499b5df4c11
ashley <ashleyyytjh@gmail.com>
2024-10-16 21:31:40 +0800

    adding js-cookie dependency for token retrieval

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,27 +1,20 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { ReactNode, useEffect, useState } from 'react'
-import { isAuthenticated } from '../services/AuthService.tsx'
 import { useAppSelector } from '@/redux/hooks.ts'
 import { authentication, logIn } from '@/redux/reducer/auth.reducer.ts'
 import Loader from './Loader.tsx'
 import { useDispatch } from 'react-redux'
+import Cookies from 'js-cookie';
 
 const ProtectedRoute = () => {
     const isAuthenticated = useAppSelector(authentication).authenticated;
     const [loading, setLoading] = useState(true); // Add loading state
     const dispatch = useDispatch();
 
-    const getCookie = async(name: string) => {
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts!.pop().split(';').shift();
-        return null;
-    }
     useEffect( () => {
-        console.log('fetching authentication data')
         const fetchData = async () => {
-            const myCookie = await getCookie('auth');
-            if (myCookie === `true`) {
+            const authValue = Cookies.get('auth');
+            if (authValue === `true`) {
                 dispatch(logIn());
             }
         }
