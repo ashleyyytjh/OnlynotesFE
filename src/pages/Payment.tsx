a4202bf1ce07545a7521a346c7fdee879261fb7b
ashley <ashleyyytjh@gmail.com>
2024-10-16 14:04:10 +0800

    fix payment page

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,15 +1,15 @@
-import getStripe from "../util/stripe.ts";
-import { Elements } from "@stripe/react-stripe-js";
+import getStripe from '../util/stripe.ts'
+import { Elements } from '@stripe/react-stripe-js'
 // import useFetchData from "../hooks/useFetchData.tsx";
-import {createOrder} from "../services/OrdersService.tsx";
+import { createOrder } from '../services/OrdersService.tsx'
 
 const tempOrder = {
-    noteId: "1",
-    buyerId: "1",
-    orderPrice: 50000
+    noteId: '1',
+    buyerId: '1',
+    orderPrice: 50000,
 }
 const Payment = () => {
-    const stripePromise = getStripe();
+    const stripePromise = getStripe()
     // const search = useSearchParams();
 
     // const {data, loading, error} = useFetchData(()=>createOrder(tempOrder));
@@ -17,24 +17,28 @@ const Payment = () => {
     const appearance = {
         theme: 'night',
         labels: 'floating',
-    };
-
-    let clientSecret: string | undefined;
-    if (data && typeof data === 'object' && 'client_secret' in data) {
-        clientSecret = (data as Record<string, unknown>)['client_secret'] as string;
-    }
-    const options = data ? {
-        clientSecret: clientSecret,
-        appearance,
-    } : {};
-
-    if (loading) {
-        // return <CircularProgress/>
     }
 
-    if (error) {
-        return <>ERROR loading Data...</>
-    }
+    let clientSecret: string | undefined
+    // if (data && typeof data === 'object' && 'client_secret' in data) {
+    //     clientSecret = (data as Record<string, unknown>)[
+    //         'client_secret'
+    //     ] as string
+    // }
+    // const options = data
+    //     ? {
+    //           clientSecret: clientSecret,
+    //           appearance,
+    //       }
+    //     : {}
+
+    // if (loading) {
+    //     // return <CircularProgress/>
+    // }
+
+    // if (error) {
+    //     return <>ERROR loading Data...</>
+    // }
 
     return (
         <></>
@@ -54,8 +58,7 @@ const Payment = () => {
         //         </Box>
         //     </Paper>
         // </Container>
-
-    );
+    )
 }
 
 export default Payment
